7fbf1792713aa0893e7e172284b4053e709bbfba
PC <PC@MAYTINH>
2024-05-24 07:40:12 +0700

    baitap3

---
+++ Helloword/src/Bai03/QLSV.java
@@ -0,0 +1,20 @@
+package Bai03;
+
+public class QLSV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SinhVien sv;
+		sv = new SinhVien();
+		// gan cac doi tuong sinh vien
+		sv.hoten = "Quy Phu";
+		sv.Namsinh = 2005;
+		sv.SDT = "0943519045";
+		System.out.print("ho ten:" + sv.hoten +  "nam sinh" + sv.Namsinh + "sdt " + sv.SDT );
+		
+		
+		
+ 
+	}
+
+}
---
+++ Helloword/src/Bai03/SinhVien.java
@@ -0,0 +1,7 @@
+package Bai03;
+
+public class SinhVien {
+		public String hoten;
+		public int Namsinh;
+		public String SDT;
+}
