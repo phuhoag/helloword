956b28104af485c42f49c7893140cd91d6f8a498
phuhoag <147980728+phuhoag@users.noreply.github.com>
2024-05-26 22:06:16 +0700

    Update XeOto.java

--- Helloword/src/edu/pxu/lthdt/bai3/XeOto.java
+++ Helloword/src/edu/pxu/lthdt/bai3/XeOto.java
@@ -2,10 +2,58 @@ package edu.pxu.lthdt.bai3;
 
 public class XeOto {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		XeOto xo = new XeOto();
+	private String hangxe;
+    private int namsanxuat;
+    private float dongco;
+
+   
+
+    public String getHangxe() {
+        return hangxe;
+    }
+
+    public int getNamsanxuat() {
+        return namsanxuat;
+    }
+
+    public float getDongco() {
+        return dongco;
+    }
+
+    public String setHangxe(String hangxe) {
+        this.hangxe = hangxe;
+        
+        return hangxe;
+    }
+
+    public int setNamsanxuat(int namsanxuat) {
+        this.namsanxuat = namsanxuat;
+        return namsanxuat;
+    }
+
+    public float setDongco(float dongco) {
+        if (dongco <= 0) {
+            System.out.println("Dong co khong the co cong suat la " + dongco + "kW");
+        } else {
+            this.dongco = dongco;
+        }
+        
+        return dongco;
+    } 
+    
+    @Override
+    public String toString() {
+        return "XeOto{" + "hangxe=" + hangxe + ", namsanxuat=" + namsanxuat + ", dongco=" + dongco + '}';
+    } 
+
+    
+    
+    public XeOto(String hangxe, int namsanxuat, float dongco) {
+        this.hangxe = setHangxe(hangxe);
+        this.namsanxuat = setNamsanxuat(namsanxuat);
+        this.dongco = setDongco(dongco);
+    }
 		
 	}
 
-}
+
