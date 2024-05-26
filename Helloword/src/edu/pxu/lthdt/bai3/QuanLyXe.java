aadcad3be5d2bda85f7ca2d6ab4fcbf81dffac5c
PC <PC@MAYTINH>
2024-05-26 22:00:10 +0700

    baitapbai3

---
+++ Helloword/src/edu/pxu/lthdt/bai3/QuanLyXe.java
@@ -0,0 +1,15 @@
+package edu.pxu.lthdt.bai3;
+
+
+public class QuanLyXe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 XeOto car = new XeOto("vinfast", 2024, 600);
+	        XeBus bus = new XeBus("transerco", 2024, 500, 30);
+	        
+	        System.out.println(car);
+	        System.out.println(bus);
+	}
+
+}
