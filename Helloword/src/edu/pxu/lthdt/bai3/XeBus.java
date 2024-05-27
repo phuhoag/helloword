54aca0a30ade06d87699e7a9f3dea7664aefd725
phuhoag <147980728+phuhoag@users.noreply.github.com>
2024-05-27 09:19:50 +0700

    Baitapbai3

--- Helloword/src/edu/pxu/lthdt/bai3/XeBus.java
+++ Helloword/src/edu/pxu/lthdt/bai3/XeBus.java
@@ -2,68 +2,71 @@ package edu.pxu.lthdt.bai3;
 
 
 public class XeBus {
-	private String hangxe;
-    private int namsanxuat;
-    private float dongco;
-    private int soghe;
-
+	
+    public class XeBus {
+private String hangxe;
+private int namsanxuat;
+private float dongco;
+private int soghe;
+public XeBus(String hangxe, int namsanxuat, float dongco, int soghe) {
+	super();
+	this.setHangxe(hangxe);
+	this.setNamsanxuat(namsanxuat);
+	this.setDongco(dongco);
+	this.setSoghe(soghe);
+}
+public String getHangxe() {
+	return hangxe;
+}
+public void setHangxe(String hangxe) {
+	this.hangxe = hangxe;
+}
+public int getNamsanxuat() {
+	return namsanxuat;
+}
+public void setNamsanxuat(int namsanxuat) { 
+	
+	LocalDate date = LocalDate.now();
+    int namhientai = date.getYear();
     
-
-    public String getHangxe() {
-        return hangxe;
+    if (namsanxuat < 1894 || namsanxuat > namhientai) {
+        System.out.println("Nam san xuat " + namsanxuat + " khong hop le.");
     }
-
-    public int getNamsanxuat() {
-        return namsanxuat;
+    this.namsanxuat = namsanxuat;
+	
+}
+public float getDongco() {
+	return dongco;
+}
+public void setDongco(float dongco) { 
+	
+	if (dongco <= 0) {
+        System.out.println("Dong co khong the co cong suat la " + dongco + "kW");
+    } else {
+        this.dongco = dongco;
     }
-
-    public float getDongco() {
-        return dongco;
-    }
-
-    public int getSoghe() {
-        return soghe;
-    }
-
-    public String setHangxe(String hangxe) {
-        this.hangxe = hangxe;
-        
-        return hangxe;
-    }
-
-    public int setNamsanxuat(int namsanxuat) {
-        this.namsanxuat = namsanxuat;
-        return namsanxuat;
-    }
-
-    public float setDongco(float dongco) {
-        if (dongco <= 0) {
-            System.out.println("Dong co khong the co cong suat la " + dongco + "kW");
-        } else {
-            this.dongco = dongco;
-        }
-        return dongco;
-    }
-
-    public int setSoghe(int soghe) {
-        if (soghe <= 0) {
-            System.out.println("So ghe " + soghe + " khong hop le.");
-        } else {
-            this.soghe = soghe;
-        }
-        return soghe;
-    }
-
-    @Override
-    public String toString() {
-        return "XeBus{" + "hangxe=" + hangxe + ", namsanxuat=" + namsanxuat + ", dongco=" + dongco + ", soghe=" + soghe + '}';
-    }
+	this.dongco = dongco;
+}
+public int getSoghe() { 
+	
+	
+	return soghe;
+}
+public void setSoghe(int soghe) { 
+	
+	 if (soghe <= 0) {
+         System.out.println("So ghe " + soghe + " khong hop le.");
+     } else {
+         this.soghe = soghe;
+     }
+	this.soghe = soghe;
+}
+@Override
+public String toString() {
+	return "XeBus [hangxe=" + hangxe + ", namsanxuat=" + namsanxuat + ", dongco=" + dongco + ", soghe=" + soghe + "]";
+}
     
-    public XeBus(String hangxe, int namsanxuat, float dongco, int soghe) {
-        this.hangxe = setHangxe(hangxe);
-        this.namsanxuat = setNamsanxuat(namsanxuat);
-        this.dongco = setDongco(dongco);
-        this.soghe = setSoghe(soghe);
-    }
+   
+    
 
 }
